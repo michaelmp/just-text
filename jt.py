4283b11e67cb9b1b2ea1351ce9b81cc74b79ea6f
Michael Morris-Pearce <mikemp@mit.edu>
2012-10-20 14:07:03 -0400

    removed old rules

--- jt.py
+++ jt.py
@@ -2,43 +2,13 @@
 
 import sys
 
-from rules import *
-
-TEMPLATE_HTML_HEADER = open('template/header.html', 'r').read()
-TEMPLATE_HTML_FOOTER = open('template/footer.html', 'r').read()
-CSS_INLINE = open('css/jt.css', 'r').read()
-CSS_SCREEN = open('css/screen.css', 'r').read()
-CSS_PRINT = open('css/print.css', 'r').read()
-
-# Order matters.
-RULES = [
-  paragraph.Rule,
-  header.Rule,
-  environment.Rule,
-  remark.Rule,
-  substitute.Rule,
-]
-
-def header():
-  env = environment.VARIABLES
-  template = TEMPLATE_HTML_HEADER
-  template = template.replace('%{css}', CSS_INLINE + CSS_SCREEN + CSS_PRINT)
-  template = template.replace('%{title}', '%s, by %s' % (env['title'], env['author']))
-  return template
-
-def footer():
-  return TEMPLATE_HTML_FOOTER
-
-def usage(args = {}):
-  return 'USAGE: %s < in.txt > out.html' % args['cmd']
+from lang.eval import *
 
 if __name__ == '__main__':
-    formatted = sys.stdin.read()
-    if len(formatted) < 1:
-      print(usage({'cmd': sys.argv[0]}))
-    else:
-      for rule in RULES:
-        formatted = rule.visit(formatted)
-      print(header())
-      print(formatted)
-      print(footer())
+  programs = [
+      open('lib/core.jt').read(),
+  ]
+  programs.append(sys.stdin.read())
+  source = '\n'.join(programs)
+  e = Evaluator()
+  print(e.evaluate(e.scan(source)))
